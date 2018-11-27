ef5e9f2e4512c96f6398894372a26b9774fbf12e
code@6110 <sky.sophia@gmail.com>
2018-11-27 11:46:46 +0800

    template build
    init new project

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+
+
+
+
+<template>
+  <div id="app">
+
+    <!-- <ul class="nav justify-content-center ">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Active</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul> -->
+
+
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style lang='scss'>
+ @import './assets/scss/main.scss'; // redesign bootstrap
+ @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
